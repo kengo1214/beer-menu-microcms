a6ab3d3cdf4720855e602de41f3ffc1e9aa4bc87
Kengo Kobayashi <kken91914@gmail.com>
2022-09-12 16:32:48 +0900

    deploy

---
+++ pages/menu/[id].js
@@ -0,0 +1,42 @@
+import styles from "../../styles/menu.module.scss";
+import { client } from "../../libs/client";
+import Link from "next/link";
+//SSG
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "menu", contentId: id });
+
+  return {
+    props: {
+      menu: data,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "menu" });
+  const paths = data.contents.map((content) => `/menu/${content.id}`);
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export default function MenuId({ menu }) {
+  return (
+    <div className={styles.body}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>{menu.title}</h1>
+        <p className={styles.publishedAt}>{menu.publishedAt}</p>
+        <div dangerouslySetInnerHTML={{ __html: `${menu.body}` }}></div>
+        <Link href="/">
+          <div>
+            <a className={styles.button}>back</a>
+          </div>
+        </Link>
+      </main>
+    </div>
+  );
+}
